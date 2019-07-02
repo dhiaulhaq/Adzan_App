1e1cf18ad1b400bd218d8f92589d478d1641f246
dhiaulhaq <dmnaufal7@gmail.com>
2019-07-03 01:49:42 +0700

    Update, masih error untuk dapetin current location (city)

--- app/src/main/java/com/example/adzan_app/MainActivity.java
+++ app/src/main/java/com/example/adzan_app/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.adzan_app;
 
+import android.Manifest;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.*;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -12,15 +17,23 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
 import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,6 +48,9 @@ public class MainActivity extends AppCompatActivity {
     TextView mShubuhTv, mDzuhurTv, mAsharTv, mMaghribTv, mIsyaTv, mLocationTv, mDateTv;
     EditText mSearchEt;
     Button mSearchBtn;
+
+    private LocationManager locationManager;
+    private String provider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,6 +84,77 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //Yang ada " /* " nya kode untuk dapetin current location dan langsun tampil di halaman utama, tapi masih error.
+
+        /*
+        //location
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        Criteria criteria = new Criteria();
+        provider = locationManager.getBestProvider(criteria, false);
+        Location location = locationManager.getLastKnownLocation(provider);
+
+        if (location != null){
+            System.out.println("Provider" + provider + " has been selected.");
+            onLocationChanged(location);
+        } else {
+
+        }
+
+        @Override
+        protected void onResume(){
+            super.onResume();
+            locationManager.requestLocationUpdates(provider, 400, 1, this);
+        } */
+
+        //getting permission for location
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            checkPermission();
+        }
+
+        //getting current location
+        FusedLocationProviderClient mFusedLocation = LocationServices.getFusedLocationProviderClient(this);
+        mFusedLocation.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+
+                if (location != null){
+                    //do it all with location
+                    Log.d("My Current Location", "Lat : " + location.getLatitude() + " Long : " + location.getLongitude());
+                    //display in toast
+                    Toast.makeText(MainActivity.this, "Lat : " + location.getLatitude() + " Long : " + location.getLongitude(), Toast.LENGTH_LONG).show();
+                }
+
+                /*
+                Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+                StringBuilder builder = new StringBuilder();
+                try{
+                    List<Address> address = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+                    int maxLines = address.get(0).getMaxAddressLineIndex();
+                    for (int i=0; i<maxLines; i++){
+                        String addressStr = address.get(0).getAddressLine(i);
+                        builder.append(addressStr);
+                        builder.append(" ");
+                    }
+
+                    String finalAddress = builder.toString();
+
+                    if (finalAddress != null) {
+                        Toast.makeText(MainActivity.this, "Location not found", Toast.LENGTH_SHORT).show();
+                    } else {
+                        //function to get location
+                        url = "https://muslimsalat.com/" + finalAddress + ".json?key=5a9f16754733539f303d99f95c51380d";
+                        searchLocation();
+                    }
+                } catch (IOException e){
+                    //handle IOException
+                } catch (NullPointerException e){
+                    // Handle NullPointerException
+                } */
+
+            }
+        });
+
         //alarm notification
         AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
         Calendar calendar = Calendar.getInstance();
@@ -77,6 +164,14 @@ public class MainActivity extends AppCompatActivity {
         PendingIntent broadcast = PendingIntent.getBroadcast(this, 100, intent, PendingIntent.FLAG_UPDATE_CURRENT);
 
         alarmManager.setExact(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), broadcast);
+    }
+
+    //permission for current location
+    private void checkPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION}, 123);
+        }
     }
 
     private void searchLocation() {
